a38c7de6b0d84f24e76981e96b5ed74af9b392ea
benjanim628 <62722423+benjanim628@users.noreply.github.com>
2020-04-01 21:17:03 +0200

    https

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; Nurturing Hope &#8212; WordPress</title>
 		<script type="text/javascript">function theChampLoadEvent(e){var t=window.onload;if(typeof window.onload!="function"){window.onload=e}else{window.onload=function(){t();e()}}}</script>
-		<script type="text/javascript">var theChampDefaultLang = 'en_US', theChampCloseIconPath = 'http://benjanim628.github.io/wp-content/plugins/super-socializer/images/close.png';var heateorSsSDKBlockedMsg = `Your browser is blocking some features of this website. Please follow the instructions at <a href=\'http://support.heateor.com/browser-blocking-social-features/\' target=\'_blank\' style=\'color:#33a9d8\'>http://support.heateor.com/browser-blocking-social-features/</a> to unblock these.`</script>
-		<script> var theChampSiteUrl = 'http://benjanim628.github.io', theChampVerified = 0, theChampEmailPopup = 0; </script>
+		<script type="text/javascript">var theChampDefaultLang = 'en_US', theChampCloseIconPath = 'https://benjanim628.github.io/wp-content/plugins/super-socializer/images/close.png';var heateorSsSDKBlockedMsg = `Your browser is blocking some features of this website. Please follow the instructions at <a href=\'http://support.heateor.com/browser-blocking-social-features/\' target=\'_blank\' style=\'color:#33a9d8\'>http://support.heateor.com/browser-blocking-social-features/</a> to unblock these.`</script>
+		<script> var theChampSiteUrl = 'https://benjanim628.github.io', theChampVerified = 0, theChampEmailPopup = 0; </script>
 			<script> var theChampFBKey = '', theChampFBLang = 'en_US', theChampFbLikeMycred = 0, theChampSsga = 0, theChampCommentNotification = 0, theChampHeateorFcmRecentComments = 0, theChampFbIosLogin = 0; </script>
-				<script> var theChampSharingAjaxUrl = 'http://benjanim628.github.io/wp-admin/admin-ajax.php', heateorSsUrlCountFetched = [], heateorSsSharesText = 'Shares', heateorSsShareText = 'Share', theChampPluginIconPath = 'http://benjanim628.github.io/wp-content/plugins/super-socializer/images/logo.png', theChampHorizontalSharingCountEnable = 0, theChampVerticalSharingCountEnable = 0, theChampSharingOffset = -10, theChampCounterOffset = -10, theChampMobileStickySharingEnabled = 0, heateorSsCopyLinkMessage = "Link copied.";
-		var heateorSsHorSharingShortUrl = "http://benjanim628.github.io/wp-login.php";var heateorSsVerticalSharingShortUrl = "http://benjanim628.github.io/wp-login.php";		</script>
+				<script> var theChampSharingAjaxUrl = 'https://benjanim628.github.io/wp-admin/admin-ajax.php', heateorSsUrlCountFetched = [], heateorSsSharesText = 'Shares', heateorSsShareText = 'Share', theChampPluginIconPath = 'https://benjanim628.github.io/wp-content/plugins/super-socializer/images/logo.png', theChampHorizontalSharingCountEnable = 0, theChampVerticalSharingCountEnable = 0, theChampSharingOffset = -10, theChampCounterOffset = -10, theChampMobileStickySharingEnabled = 0, heateorSsCopyLinkMessage = "Link copied.";
+		var heateorSsHorSharingShortUrl = "https://benjanim628.github.io/wp-login.php";var heateorSsVerticalSharingShortUrl = "https://benjanim628.github.io/wp-login.php";		</script>
 			<style type="text/css">
 	.the_champ_horizontal_sharing .theChampSharing{
 					color: #fff;
@@ -41,15 +41,15 @@
 	}
 	@media screen and (max-width:783px){.the_champ_vertical_sharing{display:none!important}}	</style>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<script type='text/javascript' src='http://benjanim628.github.io/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
-<script type='text/javascript' src='http://benjanim628.github.io/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
-<link rel='stylesheet' id='dashicons-css'  href='http://benjanim628.github.io/wp-includes/css/dashicons.min.css?ver=5.4' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='http://benjanim628.github.io/wp-includes/css/buttons.min.css?ver=5.4' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='http://benjanim628.github.io/wp-admin/css/forms.min.css?ver=5.4' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='http://benjanim628.github.io/wp-admin/css/l10n.min.css?ver=5.4' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='http://benjanim628.github.io/wp-admin/css/login.min.css?ver=5.4' type='text/css' media='all' />
-<link rel='stylesheet' id='the_champ_frontend_css-css'  href='http://benjanim628.github.io/wp-content/plugins/super-socializer/css/front.css?ver=7.12' type='text/css' media='all' />
-<link rel='stylesheet' id='the_champ_sharing_default_svg-css'  href='http://benjanim628.github.io/wp-content/plugins/super-socializer/css/share-svg.css?ver=7.12' type='text/css' media='all' />
+<script type='text/javascript' src='https://benjanim628.github.io/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script type='text/javascript' src='https://benjanim628.github.io/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<link rel='stylesheet' id='dashicons-css'  href='https://benjanim628.github.io/wp-includes/css/dashicons.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://benjanim628.github.io/wp-includes/css/buttons.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://benjanim628.github.io/wp-admin/css/forms.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://benjanim628.github.io/wp-admin/css/l10n.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://benjanim628.github.io/wp-admin/css/login.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='the_champ_frontend_css-css'  href='https://benjanim628.github.io/wp-content/plugins/super-socializer/css/front.css?ver=7.12' type='text/css' media='all' />
+<link rel='stylesheet' id='the_champ_sharing_default_svg-css'  href='https://benjanim628.github.io/wp-content/plugins/super-socializer/css/share-svg.css?ver=7.12' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -61,7 +61,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="http://benjanim628.github.io/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://benjanim628.github.io/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
@@ -79,49 +79,49 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-									<input type="hidden" name="redirect_to" value="http://benjanim628.github.io/wp-admin/" />
+									<input type="hidden" name="redirect_to" value="https://benjanim628.github.io/wp-admin/" />
 									<input type="hidden" name="testcookie" value="1" />
 			</p>
 		</form>
 
 					<p id="nav">
-									<a href="http://benjanim628.github.io/wp-login.php?action=lostpassword">Lost your password?</a>
+									<a href="https://benjanim628.github.io/wp-login.php?action=lostpassword">Lost your password?</a>
 								</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="http://benjanim628.github.io/">
+				<p id="backtoblog"><a href="https://benjanim628.github.io/">
 		&larr; Back to Nurturing Hope		</a></p>
 			</div>
-	<script type='text/javascript' src='http://benjanim628.github.io/wp-content/plugins/super-socializer/js/front/social_login/general.js?ver=7.12'></script>
-<script type='text/javascript' src='http://benjanim628.github.io/wp-content/plugins/super-socializer/js/front/facebook/sdk.js?ver=7.12'></script>
-<script type='text/javascript' src='http://benjanim628.github.io/wp-content/plugins/super-socializer/js/front/sharing/sharing.js?ver=7.12'></script>
+	<script type='text/javascript' src='https://benjanim628.github.io/wp-content/plugins/super-socializer/js/front/social_login/general.js?ver=7.12'></script>
+<script type='text/javascript' src='https://benjanim628.github.io/wp-content/plugins/super-socializer/js/front/facebook/sdk.js?ver=7.12'></script>
+<script type='text/javascript' src='https://benjanim628.github.io/wp-content/plugins/super-socializer/js/front/sharing/sharing.js?ver=7.12'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
-var _zxcvbnSettings = {"src":"http:\/\/benjanim628.github.io\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/benjanim628.github.io\/wp-includes\/js\/zxcvbn.min.js"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='http://benjanim628.github.io/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script type='text/javascript' src='https://benjanim628.github.io/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
 var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='http://benjanim628.github.io/wp-admin/js/password-strength-meter.min.js?ver=5.4'></script>
-<script type='text/javascript' src='http://benjanim628.github.io/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script type='text/javascript' src='https://benjanim628.github.io/wp-admin/js/password-strength-meter.min.js?ver=5.4'></script>
+<script type='text/javascript' src='https://benjanim628.github.io/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/mysite\/wp-admin\/admin-ajax.php"}};
 /* ]]> */
 </script>
-<script type='text/javascript' src='http://benjanim628.github.io/wp-includes/js/wp-util.min.js?ver=5.4'></script>
+<script type='text/javascript' src='https://benjanim628.github.io/wp-includes/js/wp-util.min.js?ver=5.4'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
 var userProfileL10n = {"warn":"Your new password has not been saved.","warnWeak":"Confirm use of weak password","show":"Show","hide":"Hide","cancel":"Cancel","ariaShow":"Show password","ariaHide":"Hide password"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='http://benjanim628.github.io/wp-admin/js/user-profile.min.js?ver=5.4'></script>
+<script type='text/javascript' src='https://benjanim628.github.io/wp-admin/js/user-profile.min.js?ver=5.4'></script>
 	<div class="clear"></div>
 	</body>
 	</html>
